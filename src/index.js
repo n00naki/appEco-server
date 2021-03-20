048a88867bb3a08880837c404c4351238e7b419c
Gustavo <gustavo.rocha@users.noreply.github.com>
2021-03-20 10:24:22 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const {uuid} = require('uuidv4');
+
+const app = express();
+
+// EXPLICITAR PARA O EXPRESS QUE ELE VAI TRATAR JSON
+app.use(express.json());
+
+const projects = [];
+
+app.get('/projects', (request, response) => {
+
+    return response.json(projects);
+});
+
+app.post('/projects', (request, response) => {
+
+    const {title, owner} = request.body;
+
+    const id = uuid();
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects.push(project);
+    
+    return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+
+    const { id } = request.params;
+    const {title, owner} = request.body;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+        return response.status(400).json({error: 'project not found'});
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    }
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    
+    const {id} = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+        return response.status(400).json({error: 'project not found'});
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).json([]);
+});
+
+
+app.listen(3333, () => {
+    console.log("Backend Run! :D");
+});
